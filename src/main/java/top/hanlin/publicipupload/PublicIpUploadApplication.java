dc9e45e0e8e2a2b7935006fb55a422599295aea8
linluoluo <zhouhanlin28@gmail.com>
2025-06-05 19:37:42 +0800

    项目重构

---
+++ src/main/java/top/hanlin/publicipupload/PublicIpUploadApplication.java
@@ -0,0 +1,13 @@
+package top.hanlin.publicipupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicIpUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicIpUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/hanlin/publicipupload/PublicIpUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package top.hanlin.publicipupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicIpUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
